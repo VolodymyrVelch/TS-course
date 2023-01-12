f563bf98154e009655f1f98c82aa8b0c8b3bf7cf
Volodymyr Velch <velch83@gmail.com>
2023-01-12 22:29:11 +0200

    types

--- app.ts
+++ app.ts
@@ -1,11 +1,42 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+type Page= {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: "open"|"close",
+  details?: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
 }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+
+
+const page1:Page ={
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
+}
+
+const page2:Page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
