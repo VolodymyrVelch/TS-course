632f22162681f70bf54286a70e669ba768b15f3a
Volodymyr Velch <velch83@gmail.com>
2023-01-14 21:15:20 +0200

    tsConfig

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+const page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01',
+    }
+};
+const page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
+//# sourceMappingURL=app.js.map
---
+++ src/app.js
@@ -0,0 +1,20 @@
+"use strict";
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+const page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01',
+    }
+};
+const page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+};
